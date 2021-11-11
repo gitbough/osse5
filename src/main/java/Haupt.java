b1a879f7b737e1da723d91edfeb720d77c132e5d
Joshua Gramlich <joshua.tim.gramlich@hs-furtwangen.de>
2021-11-11 09:57:06 +0100

    javaprojekt hinzugefügt

---
+++ src/main/java/Haupt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Haupt {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        input = input.toUpperCase();
+        System.out.println(input);
+    }
+}
