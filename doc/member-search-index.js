d7b370e6517084eb2685803340fe59fb267cf0c6
Joshua Gramlich <joshua.tim.gramlich@hs-furtwangen.de>
2021-11-11 10:31:15 +0100

    commit nach bearbeiten in eclipse (javadocs und reports erstellt)

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Haupt","l":"Haupt()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Haupt","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- src/main/java/Haupt.java
+++ src/main/java/Haupt.java
@@ -1,7 +1,21 @@
+/*
+ * OSSE Praktikum 5
+ * @author Joshua Gramlich
+ * @version 1.0
+ */
+
+
 import java.util.Scanner;
 
 public class Haupt {
+	/*
+	 * Main class
+	 */
     public static void main(String[] args){
+    	/*
+    	 * Gets line input from console
+    	 * prints input in uppercase
+    	 */
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         input = input.toUpperCase();
